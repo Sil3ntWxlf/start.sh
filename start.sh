6c8c27aea1b397ca5a6551e497085bba8ced6467
Sil3ntWxlf <sil3ntwxlf@gmail.com>
2021-02-03 06:26:46 -0800

    Added back "-Xms$initialRam -Xmx$maximumRam" after removing on accident.

--- start.sh
+++ start.sh
@@ -40,7 +40,7 @@ cleanUsercache="0" # remove the UUID/username cache file
 processname="java" # point this at your java binary
 
 # For most minecraft servers
-jvmArgs="-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar -Dcom.mojang.eula.agree=true"
+jvmArgs="-Xms$initialRam -Xmx$maximumRam -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar -Dcom.mojang.eula.agree=true"
 jarCmdline="--host $bindingAddress --port $serverPort --max-players $maxPlayers nogui -W:worlds" # Adding -W <folder> puts worlds there instead of in ./*
 processargs="-Dgamemode=$service -server $jvmArgs -jar $jarFile $jarCmdline"
 
